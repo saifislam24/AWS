060f473e6f819c98c15cdac2520defacd190a7a8
saif islam <saif.islam@greenwayhealth.com>
2019-08-20 14:37:27 +0530

    saif - testing protected branch

--- Startup.cs
+++ Startup.cs
@@ -21,7 +21,7 @@ namespace guid_reactapp
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<TableContext>(opt => opt.UseSqlite("Data Source=db_QandA2.db"));
